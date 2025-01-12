9e5fbfa5012bb90fb4a057c7da1f0471170c76fd
Mirzaadr <mirzaadr34@yahoo.com>
2025-01-12 17:18:44 +0700

    seeding database

--- database/seed.ts
+++ database/seed.ts
@@ -1,14 +1,15 @@
-// import { config } from "dotenv";
-// import ImageKit from "imagekit";
-// import { drizzle } from "drizzle-orm/neon-http";
-// import { neon } from "@neondatabase/serverless";
+import dummyBooks from "../dummybooks.json";
+import { config } from "dotenv";
+import ImageKit from "imagekit";
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
 
-// import { books } from "./schema";
+import { books } from "./schema";
 
-// config({ path: ".env.local" });
+config({ path: ".env" });
 
-// const sql = neon(process.env.DATABASE_URL!);
-// export const db = drizzle({ client: sql });
+const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle({ client: sql });
 
 // const dummyBooks = [
 //   {
@@ -45,56 +46,56 @@
 //   },
 // ];
 
-// const imagekit = new ImageKit({
-//   publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
-//   privateKey: process.env.IMAGEKIT_PRIVATE_KEY!,
-//   urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT!,
-// });
+const imagekit = new ImageKit({
+  publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY!,
+  urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT!,
+});
 
-// async function uploadToImageKit(url: string, fileName: string, folder: string) {
-//   try {
-//     const response = await imagekit.upload({
-//       file: url,
-//       fileName: fileName,
-//       folder,
-//     });
-//     return response.filePath;
-//   } catch (error) {
-//     console.error(`Error uploading ${fileName} to ImageKit:`, error);
-//     throw error;
-//   }
-// }
+async function uploadToImageKit(url: string, fileName: string, folder: string) {
+  try {
+    const response = await imagekit.upload({
+      file: url,
+      fileName: fileName,
+      folder,
+    });
+    return response.filePath;
+  } catch (error) {
+    console.error(`Error uploading ${fileName} to ImageKit:`, error);
+    throw error;
+  }
+}
 
-// async function seed() {
-//   console.log("Seeding books...");
+async function seed() {
+  console.log("Seeding books...");
 
-//   try {
-//     for (const book of dummyBooks) {
-//       const coverUrl = await uploadToImageKit(
-//         book.coverUrl,
-//         `${book.title}.jpg`,
-//         "/books/covers"
-//       );
+  try {
+    for (const book of dummyBooks) {
+      const coverUrl = await uploadToImageKit(
+        book.coverUrl,
+        `${book.title}.jpg`,
+        "/books/covers"
+      );
 
-//       const videoUrl = await uploadToImageKit(
-//         book.videoUrl,
-//         `${book.title}.mp4`,
-//         "/books/videos"
-//       );
+      const videoUrl = await uploadToImageKit(
+        book.videoUrl,
+        `${book.title}.mp4`,
+        "/books/videos"
+      );
 
-//       await db.insert(books).values({
-//         ...book,
-//         coverUrl,
-//         videoUrl,
-//       });
+      await db.insert(books).values({
+        ...book,
+        coverUrl,
+        videoUrl,
+      });
 
-//       console.log(`Added book: ${book.title}`);
-//     }
+      console.log(`Added book: ${book.title}`);
+    }
 
-//     console.log("Seeding completed successfully.");
-//   } catch (error) {
-//     console.error("Error seeding books:", error);
-//   }
-// }
+    console.log("Seeding completed successfully.");
+  } catch (error) {
+    console.error("Error seeding books:", error);
+  }
+}
 
-// seed();
+seed();
