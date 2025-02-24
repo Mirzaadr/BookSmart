5217d7df72818a050a8bfd1df7e827107b808586
Mirzaadr <mirzaadr34@yahoo.com>
2025-02-25 01:51:57 +0700

    add book actions for admin

--- lib/admin/actions/book.ts
+++ lib/admin/actions/book.ts
@@ -1,8 +1,15 @@
 "use server";
 
+import { auth } from "@/lib/auth";
 import { db } from "@/lib/prisma";
 
 export const createBook = async (params: BookParams) => {
+  const session = await auth();
+
+  if (!session || !session.user || session.user.role !== "ADMIN") {
+    throw new Error("You are unauthorized to do this");
+  }
+
   try {
     const newBook = await db.book.create({
       data: {
@@ -25,7 +32,36 @@ export const createBook = async (params: BookParams) => {
   }
 };
 
+export const deleteBook = async (params: { id: string }) => {
+  const session = await auth();
+
+  if (!session || !session.user || session.user.role !== "ADMIN") {
+    throw new Error("You are unauthorized to do this");
+  }
+  try {
+    const deletedBook = await db.book.delete({
+      where: { id: params.id },
+    });
+
+    return {
+      success: true,
+      data: null,
+    };
+  } catch (error) {
+    console.error(JSON.stringify(error));
+
+    return {
+      success: false,
+      error: "An error occured while Deleting the book",
+    };
+  }
+};
 export const updateBook = async (params: BookParams & { id: string }) => {
+  const session = await auth();
+
+  if (!session || !session.user || session.user.role !== "ADMIN") {
+    throw new Error("You are unauthorized to do this");
+  }
   try {
     const updatedBook = await db.book.update({
       where: { id: params.id },
